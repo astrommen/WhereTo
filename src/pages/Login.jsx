8c205fafd0fa924b9beb6ce195090e5f08413997
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-05-12 21:01:57 -0400

    google signOn btn works

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Wrapper from "../components/Wrapper";
+import SignOn from "../components/SignOn";
 
 class Login extends Component {
 
@@ -8,6 +9,7 @@ class Login extends Component {
     return (
       <Wrapper>
         LOGIN
+        <SignOn />
       </Wrapper>
     );
   }
