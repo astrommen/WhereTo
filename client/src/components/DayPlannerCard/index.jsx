63da41bd8dab06683d31f26dd3fe9c5f84fcd4ce
hgowton <hgowton@gmail.com>
2020-05-16 21:13:47 -0400

    working on DayPlan Card

--- client/src/components/DayPlannerCard/index.jsx
+++ client/src/components/DayPlannerCard/index.jsx
@@ -17,8 +17,13 @@ const Title = styled.h3 `
 color: white;
 `
 
-const LocDes = styled.p`
-color: white;`
+const LocDes = styled.p `
+color: white;
+font-weight: bold;`
+
+const White = styled.p `
+color: whilte;
+`
 
 function DayPlannerCard(props) {
   return(
@@ -30,6 +35,12 @@ function DayPlannerCard(props) {
       <Row>
         <Col size="md-12"><LocDes>{props.locDescription}</LocDes></Col>
       </Row>
+      {/* <Row>
+        <Col size="md-6"><White>First Day: {props.days[0].date} - Last Day: {props.days[props.days.length-1].date}</White>
+        </Col>
+        <Col size="md-6"><White>
+          props</White></Col>
+      </Row> */}
       {/* {props.dayplans.map(day => {
 
       })} */}
