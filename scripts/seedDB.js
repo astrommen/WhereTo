dd48bfc6bc070a0a2e0d638b70f658f9409a3ec5
Bill Kamberos <bill.kamberos@gmail.com>
2020-05-19 22:26:09 -0400

    Changed getUser routing

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,30 +1,103 @@
 const mongoose = require("mongoose");
 const db = require("../models");
+var async = require('async');
 
-// This file empties the Books collection and inserts the books below
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-  "mongodb://localhost/whereto"
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/whereto", { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
+
+
 
 const userSeed = [
   {
     name: "Bill",
     email: "bill@bill.com",
     password: "waewfeawf",
-    password2: "waewfeawf",
+    vacations: []
   }
 ];
 
+const vacaSeed = [
+  {
+    "email": "bill@bill.com",
+    "name": "Vaawdawddver!",
+    "startDate": "2019-12-2",
+    "endDate": "2019-12-6",
+    "location": "Alaska",
+    "activities": ["camping", "biking", "hiking", "fishing"]
+  },
+  {
+    "email": "bill@bill.com",
+    "name": "Future!",
+    "startDate": "2020-12-2",
+    "endDate": "2020-12-6",
+    "location": "Alaska",
+    "activities": ["camping", "biking", "hiking", "fishing"]
+  },
+  {
+    "email": "bill@bill.com",
+    "name": "Vaawdawddver1!",
+    "startDate": "2019-12-2",
+    "endDate": "2019-12-6",
+    "location": "Alaska",
+    "activities": ["camping", "biking", "hiking", "fishing"]
+  },
+  {
+    "email": "bill@bill.com",
+    "name": "Vaawdawddver2!",
+    "startDate": "2019-12-2",
+    "endDate": "2019-12-6",
+    "location": "Alaska",
+    "activities": ["camping", "biking", "hiking", "fishing"]
+  },
+]
+
 db.User
-  .remove({})
+  .deleteMany({})
   .then(() => db.User.collection.insertMany(userSeed))
   .then(data => {
-    console.log(data.result.n + " records inserted!");
+    console.log(data);
     process.exit(0);
   })
   .catch(err => {
     console.error(err);
     process.exit(1);
   });
+
+
+db.Vacation
+  .deleteMany({})
+  .then(data => {
+    // console.log(data)
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+
+
+// async.each(vacaSeed, function (data, callback) {
+//   db.Vacation
+//     .create(data)
+//     .then(dbModel => {
+//       console.log(data)
+//       db.User.findOneAndUpdate({ email: data.email }, { $push: { vacations: dbModel._id } }, { new: true })
+//     })
+//     .catch(err => console.log(err));
+// })
+
+
+
+vacaSeed.forEach(data => {
+  // console.log(data)
+  db.Vacation
+    .create(data)
+    .then(dbModel => {
+      console.log(dbModel)
+      console.log(data)
+      db.User.findOneAndUpdate({ email: data.email }, { $push: { vacations: dbModel._id } }, { new: true })
+    })
+    .catch(err => console.log(err));
+
+})
