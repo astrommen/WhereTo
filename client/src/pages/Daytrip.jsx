514a5b9ea1bb03816b49035aa74494c6e7709298
hgowton <hgowton@gmail.com>
2020-05-24 18:09:50 -0400

    loading and no images

--- client/src/pages/Daytrip.jsx
+++ client/src/pages/Daytrip.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import Nav from "../components/Nav"
-import { Title, Wrapper } from "../components/Styled";
-import { withRouter } from 'react-router-dom'
-import TripData from "../components/TripData";
-
+import {Title, Wrapper} from "../components/Styled";
 
 class DayTrip extends Component {
   constructor(props) {
@@ -26,29 +23,8 @@ class DayTrip extends Component {
   render() {
     return (
       <Wrapper>
-        <Nav
-          vacaId={localStorage.getItem('vacaId')}
-        />
-        <TripData
-          vacaId={localStorage.getItem('vacaId')}
-        // tripName={this.state.tripName}
-        // dateStart={this.state.dateStart}
-        // city={this.state.city}
-        // state={this.state.state}
-        // boating=""
-        // fishing=""
-        // hiking=""
-        // beach=""
-        // concert=""
-        // sports=""
-        // theatre=""
-        // sightseeing=""
-        // breakfast=""
-        // dinner=""
-        // dessert=""
-        // drinks=""
-        // foodType=""
-        />
+        <Nav />
+        <Title>Start Compiling your Day Trip</Title>
       </Wrapper>
     );
   }
--- controllers/ticketmasterController.js
+++ controllers/ticketmasterController.js
@@ -2,10 +2,10 @@ const axios = require("axios");
 const db = require("../models");
 
 module.exports = {
-    findAll: function (req, res) {
-        const { query: params } = req;
-        let url = `https://app.ticketmaster.com/discovery/v2/events?classificationName=[${params.activity}]&radius=${params.distance}&unit=miles&startDateTime=${params.dateStart}&city=${params.city}&state=${params.state}&apikey=${process.env.REACT_APP_TM_APIKEY}`
-        console.log(process.env.REACT_APP_TM_APIKEY)
+    findAll: function(req, res) {
+        const {query: params} = req;
+        let url =`https://app.ticketmaster.com/discovery/v2/events?classificationName=${params.activity}&radius=${params.distance}&unit=miles&startDateTime=${params.dateStart}T00:00:00Z&city=${params.city}&apikey=${process.env.REACT_APP_TM_APIKEY}`
+        console.log(url)
         axios
             .get(url)
             .then(results => {
