bc97eba7b020c91c600d03a8270114be43396792
Aja <a.g.mechanix>
2020-05-27 18:50:18 -0400

    got the facebook user pushed to db

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const app = express();
 const keys = require("./config/keys");
 const PORT = process.env.PORT || 3001;
 
+const User = require("./models/user");
 // passport facebook
 passport.use(new FacebookStrategy({
   clientID: keys.facebook.clientID,
@@ -25,9 +26,31 @@ passport.use(new FacebookStrategy({
   callbackURL: 'http://localhost:3001/auth/facebook/callback',
   profileFields: ['id', 'email', 'name', 'photos'] 
 
-},
-function(accessToken, refreshToken, profile, cb) {
-  console.log("unique", accessToken, refreshToken, profile)
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    const userProfile = profile._json;
+    console.log("unique", accessToken, userProfile);
+    console.log(userProfile.picture.data.url);
+    // app.post("/register", (req, res) => {
+      User.findOne({ email: userProfile.email }).then(user => {
+        console.log("user");
+        if (user) {
+          console.log({email: "Email already exists "});
+          return cb(null, user);
+        } else {
+          const newUser = new User({
+            name: userProfile.first_name + " " + userProfile.last_name,
+            email: userProfile.email,
+            password: Date.now()+userProfile.id,
+            profle_pic: userProfile.picture.data.url
+          });
+          newUser.save()
+          // .then(user => res.json(user)).catch(err => console.log(err));
+          console.log("user added");
+        }
+      });
+    // });
+  // User.findOrCreate({ facebook})
   return cb(null, profile);
 }));
 
@@ -75,7 +98,7 @@ app.all('/*', function(req, res, next) {
 });
 
 app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }), function(req,res){
-  console.log(profile);
+  console.log("this is profile json", profile._json);
 });
 
 // { 
@@ -87,14 +110,14 @@ app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }),
 // );
 
 //step 1. save user info to db
-//step 2. set and check for jwt: base64 hash the combined userid+email to create jwt
-
+//step 2. set and check for jwt: base64hash the combined userid+email to create jw
 app.get('/auth/facebook/callback', 
   passport.authenticate('facebook', 
   { successRedirect: 'http://localhost:3000/profile',failureRedirect: '/login' },
 
   ),
     function(req, res) {
+      
       window.location.href="http://localhost:3000/profile";
     }
   );
@@ -104,14 +127,6 @@ app.get('/auth/facebook/callback',
 //     console.log("redirected");
 //   }
 // );
-
-app.get('/auth/google', passport.authenticate('google'));
-
-app.get('/return', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/profile');
-});
 
 // Passport config
 require("./config/passport")(passport);
